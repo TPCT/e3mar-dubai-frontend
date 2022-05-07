c01f131eda1a9fed19a7f212fdc485cf08245689
TPCT <eslamjackie@outlook.com>
2022-05-07 15:18:55 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,9 +5,41 @@ $(function(){
         touchControls: true,
         gyroControls: false,
         scale: 1.00,
-        scaleMobile: 1.00
+        scaleMobile: 1.00,
+        color: 0xdc7f00,
+        backgroundColor: 0x0
       });
-    $('.header-item').hover(function (event){
-        console.log(event.target);
-    })
+
+    let canvas = $('#header-canvas');
+    let context = canvas[0].getContext('2d');
+    
+    function drawLine(x1, y1, x2, y2, width='1', color='#DAA520'){
+        context.beginPath();
+        context.lineWidth=width;
+        context.strokeStyle=color;
+        context.moveTo(x1, y1);
+        context.lineTo(x2, y2);
+        context.stroke();
+    }
+
+    function connectNodes(selector){
+        context.clearRect(0, 0, canvas.width(), canvas.height());
+        let points = [];
+        $(selector).each(function(index, item){
+            let item_position = $(this).offset();
+            item_position.left += $(this).width() / 2;
+            item_position.top += $(this).height() / 2;
+            points.push(item_position);
+        });
+
+        for(let i = 0; i < points.length; i++){
+            for (let j=0; j < points.length; j++){
+                drawLine(points[i].left, points[i].top, points[j].left, points[j].top)
+            }
+        }
+    }
+
+    $(window).resize(function(){connectNodes('.header-item');})
+    connectNodes('.header-item');
+    
 });
